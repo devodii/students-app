5ee24449f7f14ee1c8c4f26102ae168fa62046ae
Emmanuel Odii <emmanuelodii80@gmail.com>
2024-02-06 18:42:16 +0100

    chore: include links on page

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
 import { Wrapper } from "../components/wrapper";
 
 export default function App() {
   return (
     <Wrapper>
-      <div className="text-4xl font-semibold">Betalist</div>
+      <ul className="grid grid-cols-1 gap-4">
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/sign-in">SignIn</Link>
+        </li>
+        <li>
+          <Link to="/sign-up">SignUp</Link>
+        </li>
+        <li>
+          <Link to="/domain">Domain</Link>
+        </li>
+      </ul>
     </Wrapper>
   );
 }
