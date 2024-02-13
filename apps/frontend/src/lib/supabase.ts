6de1650faca7349f3ad59923c07ba32242647ce4
Emmanuel Odii <emmanuelodii80@gmail.com>
2024-02-13 17:24:36 +0100

    chore: setup environment variables

--- apps/frontend/src/lib/supabase.ts
+++ apps/frontend/src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = "https://gettcwogbfbvxulprltq.supabase.co";
-const anonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdldHRjd29nYmZidnh1bHBybHRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3ODE1MjAsImV4cCI6MjAyMzM1NzUyMH0.I70dhh81Zk9g1iS4N7OiDy3xs9OmbOy_x2_2mz8jGFw";
-
-export default createClient(url, anonKey);
+export default createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON
+);
